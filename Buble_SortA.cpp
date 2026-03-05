c5b76a118dfec4eaf1294fb25394f7edd9460038
muhammadfahrezarahman-code <anwarfahreza4@gmail.com>
2026-03-05 15:41:53 +0700

    memasukkan void  bubble sort Array

--- Buble_SortA.cpp
+++ Buble_SortA.cpp
@@ -20,9 +20,45 @@ void input()
         }
     }
 }
-cout << endl;
-cout << "====================" << endl;
-cout << "Masukan Elemen Array" << endl;
-cout << "====================" << endl;
+        cout << endl;
+        cout << "====================" << endl;
+        cout << "Masukan Elemen Array" << endl;
+        cout << "====================" << endl;
 
 for (int i = 0; i < n; i++)
+{
+    cout << "Data ke-" << (i + 1) << ": ";
+    cin >> arr[i];
+}
+
+
+
+
+
+void bubblesortArray()
+{
+    int pass = 1;
+    
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp; 
+            }
+        }
+        pass += 1;
+
+        cout << "\nPass " << pass - 1 << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+        cout << endl;
+
+    } while (pass <= n - 1);
+}
