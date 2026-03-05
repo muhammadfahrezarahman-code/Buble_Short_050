4e324e323b5a04228696143020d93eda178d6401
muhammadfahrezarahman-code <anwarfahreza4@gmail.com>
2026-03-05 15:50:09 +0700

    memasukkan void display

--- Buble_SortA.cpp
+++ Buble_SortA.cpp
@@ -16,7 +16,7 @@ void input()
             break;
         else
         {
-            COUT << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+            cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
         }
     }
 }
@@ -61,4 +61,27 @@ void bubblesortArray()
         cout << endl;
 
     } while (pass <= n - 1);
+}
+
+void display()
+{
+    cout << endl;
+    cout << "==================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "==================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j];
+        if (j < n - 1)
+        {
+            cout << " -->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl;
 }
